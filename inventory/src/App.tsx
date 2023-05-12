c388426ce2bca21a21cd1d137fc478cb6eb984fd
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-05-12 13:57:39 -0400

    -

--- inventory/src/App.tsx
+++ inventory/src/App.tsx
@@ -1,7 +1,6 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Test from './pages/test/Test';
-import upcScanner from './pages/upcScanner/UpcScan';
 
 function App() {
   return (
@@ -9,9 +8,8 @@ function App() {
       <div className="h-16 shadow-2xl bg-primary flex items-center justify-center font-extrabold">
         ACCESS PHARMACIES INVENTORY TOOL
       </div>
-
       <Routes>
-        <Route path="upcScanner" Component={upcScanner}></Route>
+        <Route path="upcScanner"></Route>
         <Route path="test" Component={Test} />
       </Routes>
       <div></div>
