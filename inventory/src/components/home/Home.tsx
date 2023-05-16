db64ac83cee8d02d66a10a48d1e7ef62dcff44ac
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-05-16 16:46:02 -0400

    -

--- inventory/src/components/home/Home.tsx
+++ inventory/src/components/home/Home.tsx
@@ -95,8 +95,22 @@ export default function Home() {
             className="card glass bg-primary-content hover:bg-secondary shadow-2xl"
             style={{ width: '100%', height: '0', paddingBottom: '100%' }}
           >
-            <div className="card-body">
-              <h2 className="card-title">Profile</h2>
+            <div className="card-body items-center">
+              <h2 className="card-title text-center">Profile</h2>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-1/2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
 
               <div className="card-actions justify-end"></div>
             </div>
