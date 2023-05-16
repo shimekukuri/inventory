4999c98a4701090016701a5ae07ca55b76c87e27
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-05-16 10:32:28 -0400

    -

--- inventory/src/components/home/Home.tsx
+++ inventory/src/components/home/Home.tsx
@@ -3,33 +3,64 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="flex-1 flex">
-      <div className="flex-1 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <div className="flex justify-center items-center p-4">
-          <div className="card">
-            <div className="card glass">
-              <div className="card-body">
-                <h2 className="card-title">Life hack</h2>
-                <p>How to park your car at your garage?</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Learn now!</button>
-                </div>
-              </div>
+      <div className="flex-1 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+        <div className="flex justify-center items-center ">
+          <div
+            className="card glass bg-primary hover:bg-secondary"
+            style={{ width: '100%', height: '0', paddingBottom: '100%' }}
+          >
+            <div className="card-body">
+              <h2 className="card-title">Life hack</h2>
+
+              <div className="card-actions justify-end"></div>
             </div>
           </div>
         </div>
-        <div>test2</div>
-        <div>test3</div>
-        <div>test4</div>
-        <div className="flex justify-center items-center p-4">
-          <div className="card">
-            <div className="card glass">
-              <div className="card-body">
-                <h2 className="card-title">Life hack</h2>
-                <p>How to park your car at your garage?</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Learn now!</button>
-                </div>
-              </div>
+        <div className="flex justify-center items-center">
+          <div
+            className="card glass bg-primary hover:bg-secondary"
+            style={{ width: '100%', height: '0', paddingBottom: '100%' }}
+          >
+            <div className="card-body">
+              <h2 className="card-title">Life hack</h2>
+
+              <div className="card-actions justify-end"></div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div
+            className="card glass bg-primary hover:bg-secondary"
+            style={{ width: '100%', height: '0', paddingBottom: '100%' }}
+          >
+            <div className="card-body">
+              <h2 className="card-title">Life hack</h2>
+
+              <div className="card-actions justify-end"></div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div
+            className="card glass bg-primary hover:bg-secondary"
+            style={{ width: '100%', height: '0', paddingBottom: '100%' }}
+          >
+            <div className="card-body">
+              <h2 className="card-title">Life hack</h2>
+
+              <div className="card-actions justify-end"></div>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <div
+            className="card glass bg-primary hover:bg-secondary"
+            style={{ width: '100%', height: '0', paddingBottom: '100%' }}
+          >
+            <div className="card-body">
+              <h2 className="card-title">Life hack</h2>
+
+              <div className="card-actions justify-end"></div>
             </div>
           </div>
         </div>
